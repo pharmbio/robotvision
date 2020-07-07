69b1e215d27537ad58ef03f6a7f44abc3528e1fa
rikard <rikardnystrom95@gmail.com>
2020-07-07 20:30:59 +0200

    test image

--- test.py
+++ test.py
@@ -1,17 +1,43 @@
-from imutils.video import VideoStream
+import numpy as np
 import cv2
+import imutils
 
-vs = VideoStream(src=0).start()
+def nothing(x):
+    pass
+
+
+cv2.namedWindow("Trackbars")
+hh = "Max"
+hl = "Min"
+wnd = "Colorbars"
+
+cv2.createTrackbar("Threshold", "Trackbars", 150, 255, nothing)
+cv2.createTrackbar("HoughLines", "Trackbars", 255, 255, )
+
 
 while True:
-    frame = vs.read()
+    # Open image and make copy of original
+    img = cv2.imread("20200707_174026.jpg")
+    orig = img.copy()
+    img = imutils.resize(img, height = 600)
 
-    cv2.imshow("Test", frame)
+    # Get slider variables
+    l_v = cv2.getTrackbarPos("Threshold", "Trackbars")
+    u_v = cv2.getTrackbarPos("HoughLines", "Trackbars")
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+    # Convert to grayscale, remove high freq noise
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 11, 17, 17)
 
-print("[INFO] Cleaning up...")
-cv2.destroyAllWindows()
-vs.stop()
+
+
+ret, gray = cv2.threshold(gray, l_v, 255, cv2.THRESH_BINARY)
+
+edged = cv2.Canny(gray, 30, 200)
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+cv2.imshow("Test", edged)
+cv2.waitKey(0)
--- webcam.py
+++ webcam.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import imutils
 
 cap = cv2.VideoCapture(0)
 
