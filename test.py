7feb7b49aee9c1fd7abf2520850d1d08b432cf68
rikard <rikardnystrom95@gmail.com>
2020-07-08 16:09:30 +0200

    Image processing started

--- test.py
+++ test.py
@@ -1,43 +1,31 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use("TkAgg")
 import cv2
 import imutils
 
-def nothing(x):
-    pass
+# Open image and make copy of original
+img = cv2.imread("20200707_174026.jpg")
+orig = img.copy()
+img = imutils.resize(img, height = 600)
 
+# Convert to grayscale, remove high freq noise
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
 
-cv2.namedWindow("Trackbars")
-hh = "Max"
-hl = "Min"
-wnd = "Colorbars"
-
-cv2.createTrackbar("Threshold", "Trackbars", 150, 255, nothing)
-cv2.createTrackbar("HoughLines", "Trackbars", 255, 255, )
-
-
-while True:
-    # Open image and make copy of original
-    img = cv2.imread("20200707_174026.jpg")
-    orig = img.copy()
-    img = imutils.resize(img, height = 600)
-
-    # Get slider variables
-    l_v = cv2.getTrackbarPos("Threshold", "Trackbars")
-    u_v = cv2.getTrackbarPos("HoughLines", "Trackbars")
-
-    # Convert to grayscale, remove high freq noise
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+# OTSU thresholding
+(ret, thr) = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+thr2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
+    cv2.THRESH_BINARY, 11, 8)
 
 
 
-ret, gray = cv2.threshold(gray, l_v, 255, cv2.THRESH_BINARY)
 
-edged = cv2.Canny(gray, 30, 200)
-cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-screenCnt = None
 
-cv2.imshow("Test", edged)
-cv2.waitKey(0)
+# Plotting
+plt.subplot(2,2,1), plt.imshow(orig)
+plt.subplot(2,2,2), plt.imshow(gray,    "gray")
+plt.subplot(2,2,3), plt.imshow(thr,     "gray")
+plt.subplot(2,2,4), plt.imshow(thr2,    "gray")
+plt.show()
